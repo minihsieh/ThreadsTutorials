0f546a06fd49939a3bd61560d38ea3489ce470de
MiniHsieh <mini.hsieh@tpisoftware.com>
2024-06-27 14:43:11 +0800

    Github Action add file to .github/deployment

--- ThreadsTutorials/Core/Root/View/ContentView.swift
+++ ThreadsTutorials/Core/Root/View/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         Group {
-            if viewModel.userSession != nil {
+            if $viewModel.userSession != nil {
                 ThreadsTabView()
             } else {
                 LoginView()
